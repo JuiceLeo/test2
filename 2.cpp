bcf52384b271fb52b2d7df4eb55d097f18f3149d
lfy <412799071@qq.com>
2019-03-14 21:22:46 +0800

    ubuntu create a file ,synchro to github

---
+++ 2.cpp
@@ -0,0 +1,15 @@
+/*********************************************************
+	> File Name:2.cpp
+	> Author: 
+	>Mail: 
+	> Created Time:2019年03月14日 星期四 21时21分02秒
+*****************************************************/
+ 
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+ 	cout << "this is client ubuntu create a file" << endl;
+}
